b596d3045a1f7b132204e253c0c03d49b912a86e
AIGautam <kumargautam.gk0039@gmail.com>
2025-02-20 12:27:54 +0530

    Add Chatsection

--- src/components/chatSection/ChatSection.jsx
+++ src/components/chatSection/ChatSection.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import "./ChatSection.css";
 import Darkmode from '../Darkmode/Darkmode';
+import { FiSend } from "react-icons/fi";
+
 function ChatSection() {
-  return (
-    <div className="chatsection">
-      <Darkmode/>
-    </div>
-  );
+    return (
+        <div className="chatsection">
+            <div className="topsection">
+                <div className="headings">
+                    <span>HELLO GAUTAM</span>
+                    <span>I'm Your Own Assistant</span>
+                    <span>What can I help you...?</span>
+                </div>
+            </div>
+            <div className="bottomsection">
+                <input type="text" placeholder='Enter a prompt' />
+                <button id='sentbtn'><FiSend />
+                </button>
+                <Darkmode/>
+            </div>
+        </div>
+    );
 }
 
 export default ChatSection;
